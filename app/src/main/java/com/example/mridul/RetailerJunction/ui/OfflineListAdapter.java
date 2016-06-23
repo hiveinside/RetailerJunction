c8af6ef104cae48bebacee5199ee5a1ec7205b01
Mridul Gupta <mridul.gupta@lavainternational.in>
2016-06-23 20:28:00 +0530

    added star rating view in offline view

--- app/src/main/java/com/example/mridul/RetailerJunction/ui/OfflineListAdapter.java
+++ app/src/main/java/com/example/mridul/RetailerJunction/ui/OfflineListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.mridul.RetailerJunction.ui;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,8 @@ import android.widget.TextView;
 
 import com.example.mridul.RetailerJunction.utils.AppInfoObject;
 import com.example.mridul.helloworld.R;
+
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.util.List;
@@ -45,16 +48,46 @@ public class OfflineListAdapter extends ArrayAdapter<AppInfoObject> {
         File imgFile = new File(RetailerApplication.getIconDir() + appInfo.packageName + ".png");
         if(imgFile.exists()) {
             icon.setImageURI(Uri.fromFile(imgFile));
+        } else {
+            icon.setImageResource(R.drawable.def);
         }
 
         TextView title = (TextView) one_row.findViewById(R.id.appTitle);
         title.setText(appInfo.appName);
 
         TextView detail = (TextView) one_row.findViewById(R.id.appDetail);
-        detail.setText(appInfo.packageName + "  |  " + appInfo.size);
+
+        Drawable image = context.getResources().getDrawable(getRatingDrawable(appInfo.rating));
+        image.setBounds( 0, 0, image.getIntrinsicWidth()/3, image.getIntrinsicHeight()/3 );
+        detail.setCompoundDrawables( image, null, null, null );
+
+        detail.setText(" (" + FileUtils.byteCountToDisplaySize(appInfo.size) + ")");
 
         return one_row;
     }
 
+    private int getRatingDrawable(float rating) {
 
+        int id;
+        if (rating > 4.5)
+            id = R.drawable.stars_5;
+        else if (rating > 4.0)
+            id = R.drawable.stars_4_5;
+        else if (rating > 3.5)
+            id = R.drawable.stars_4;
+        else if (rating > 3.0)
+            id = R.drawable.stars_3_5;
+        else if (rating > 2.5)
+            id = R.drawable.stars_3;
+        else if (rating > 2.0)
+            id = R.drawable.stars_2_5;
+        else if (rating > 1.5)
+            id = R.drawable.stars_2;
+        else if (rating > 1.0)
+            id = R.drawable.stars_1_5;
+        else
+            id = R.drawable.stars_1;
+
+        return id;
+    }
 }
